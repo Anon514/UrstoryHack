74a4225546df47556d4fc92773174cf8d9912248
Joshua Karol <josh@themistechnology.com>
2017-11-10 20:09:12 -0500

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,45 @@
+import { NgModule, ErrorHandler } from '@angular/core';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+import { Signup } from '../pages/signup/signup';
+import { Login } from '../pages/login/login';
+import { Home } from '../pages/home/home';
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { AngularFireModule } from 'angularfire2';
+import { AuthService } from '../providers/auth-service';
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyDQdcFOv5-WDn_GUn-4CxvjWzWW5JbIDu8",
+    authDomain: "urstory-7f855.firebaseapp.com",
+    databaseURL: "https://urstory-7f855.firebaseio.com/",
+    storageBucket: "urstory-7f855.appspot.com",
+    messagingSenderId: "563705458949"
+};
+
+@NgModule({
+  declarations: [
+    MyApp,
+    Signup,
+    Home,
+    Login
+  ],
+  imports: [
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    Signup,
+    Home,
+    Login
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    AuthService
+  ]
+})
+export class AppModule {}
---
+++ src/app/models/user.ts
@@ -0,0 +1,20 @@
+import { AngularFire, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2';
+import { AuthService } from '../../providers/auth-service';
+
+export class User {
+    first: string;
+    email: string;
+    last: string;
+    handle: string;
+    birthday: string;
+    public af: AngularFire;
+
+    constructor(first: string, email: string,
+        last: string, handle: string, birthday: string) {
+        this.first = first;
+        this.last = last;
+        this.email = email;
+        this.handle=handle;
+        this.birthday=birthday;
+    }
+}
